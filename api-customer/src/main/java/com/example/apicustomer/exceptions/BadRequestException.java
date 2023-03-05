af6d33e929e4fbc26848f9c3ebb63afd74d52ff8
JulianaAparicio <juliana_aparicio@outlook.com>
2023-03-05 17:07:26 -0300

    Added exceptions

---
+++ api-customer/src/main/java/com/example/apicustomer/exceptions/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.example.apicustomer.exceptions;
+
+public class BadRequestException extends Exception{
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+}
