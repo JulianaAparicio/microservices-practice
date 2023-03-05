74cbcefba14acd47847ad0e152ebd6a527bd2463
JulianaAparicio <juliana_aparicio@outlook.com>
2023-03-05 20:34:19 -0300

    Added DTO to wallet

---
+++ api-wallet/src/main/java/com/example/apiwallet/dtos/WalletDTO.java
@@ -0,0 +1,11 @@
+package com.example.apiwallet.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WalletDTO {
+    private Long id;
+    private Double balance;
+}
