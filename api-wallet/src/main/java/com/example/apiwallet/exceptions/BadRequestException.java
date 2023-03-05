3f2707945c983a923e092d0f78c86fbbbd140669
JulianaAparicio <juliana_aparicio@outlook.com>
2023-03-05 20:32:32 -0300

    Added exceptions to wallet microservice

---
+++ api-wallet/src/main/java/com/example/apiwallet/exceptions/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.example.apiwallet.exceptions;
+
+public class BadRequestException extends Exception{
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+}
